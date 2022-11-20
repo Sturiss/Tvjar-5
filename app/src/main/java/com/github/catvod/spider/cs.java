400850a91ab8c3ea88cac38377c29bcd0812fcfc
asters1 <97214636+asters1@users.noreply.github.com>
2022-11-20 19:27:32 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/catvod/spider/cs.java
@@ -0,0 +1,26 @@
+package com.github.catvod.spider;
+
+import android.content.Context;
+
+import com.github.catvod.crawler.Spider;
+
+
+/**
+ * Demo for spider
+ * <p>
+ * Author: CatVod
+ */
+public class cs extends Spider {
+    @Override
+    public void init(Context context) {
+        super.init(context);
+    }
+
+    public String homeContent(boolean filter) {
+
+
+        return "{\"class\":[{\"type_id\":\"1\",\"type_name\":\"电影\"},{\"type_id\":\"2\",\"type_name\":\"连续剧\"},{\"type_id\":\"3\",\"type_name\":\"综艺\"},{\"type_id\":\"4\",\"type_name\":\"动漫\"}]";
+
+
+    }
+}
