799141959c2b3cc73e210ea7c22401ff5d4dd453
asters1 <97214636+asters1@users.noreply.github.com>
2022-11-20 22:00:56 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/HOME.java
+++ app/src/main/java/com/github/catvod/spider/HOME.java
@@ -16,6 +16,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -47,22 +49,77 @@ public class HOME extends Spider {
                 // 获取文本信息
                 jsonObject.put("type_name", links.get(i).text().split("/")[0]);
                 classes.put(jsonObject);
-
-
                 result.put("class",classes);
             }
-
           // System.out.println("=============："+classes);
+            return result.toString();
+        }catch(Exception e){
+            SpiderDebug.log(e);
+        }
+        return "";
+        }
+
+    public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
+        try {
+            JSONObject result = new JSONObject();
+            JSONArray list = new JSONArray();
+            result.put("page", 1);
+            result.put("pagecount", 1);
+            String curl = siteUrl + "/" + tid;
+            String con_html = OkHttpUtil.string(curl, null);
+            Document con_doc = Jsoup.parse(con_html, "UTF-8");
+            Elements links = con_doc.getElementsByTag("a");
+            result.put("limit",links.size()-1);
+            result.put("total",links.size()-1);
+            for (int i = 1; i < links.size(); i++) {
+                JSONObject jsonObject = new JSONObject();
+                // 获取超链接路径
+                jsonObject.put("vod_id", tid+"/"+links.get(i).attr("href"));
+                // 获取文本信息
+                jsonObject.put("vod_name", links.get(i).text().split("/")[0]);
+                list.put(jsonObject);
+                result.put("list", list);
+            }
+
+            return result.toString();
+        }catch(Exception e){
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+
+
+    public String detailContent(List<String> ids) {
+        try {
+            JSONObject result = new JSONObject();
+            JSONArray list = new JSONArray();
+            String durl = siteUrl + "/" + ids.get(0);
+            String don_html = OkHttpUtil.string(durl, null);
+            Document don_doc = Jsoup.parse(don_html, "UTF-8");
+            Elements links = don_doc.getElementsByTag("a");
+            JSONObject info = new JSONObject();
+            info.put("vod_id",ids.get(0));
+            info.put("vod_name",ids.get(1));
+            info.put("vod_pic","");
+            String play_from = "";
+            for (int i = 1; i < links.size(); i++) {
+                if(i==links.size()-1){
+                    play_from=play_from+links.get(i).text().split("/")[0];
+                }else {
+                    play_from=play_from+links.get(i).text().split("/")[0]+"$$$";
+                }
+
+                 }
+            info.put("vod_play_from",play_from);
+            list.put(info);
+            result.put("list",list);
             return result.toString();
         }catch(Exception e){
             SpiderDebug.log(e);
         }
 
 
-
         return "";
-        }
-
-
-
+    }
 }
+
