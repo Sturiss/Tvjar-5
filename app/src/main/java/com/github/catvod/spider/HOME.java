f03149266b35f134bc81bedb733aac8e5d355ed3
asters1 <gj744559140@gmail.com>
2022-11-20 18:35:55 +0800

    nginx

--- app/src/main/java/com/github/catvod/spider/HOME.java
+++ app/src/main/java/com/github/catvod/spider/HOME.java
@@ -33,8 +33,8 @@ import java.util.regex.Pattern;
  */
 public class HOME extends Spider {
 
-    private static final String siteUrl = "http://192.168.1.1:8888";
-    private static final String siteHost = "192.168.1.1:8888";
+    private static final String siteUrl = "http://localhost:8080";
+    private static final String siteHost = "localhost:8080";
 
     private Pattern re_fenglei = Pattern.compile("<pre>(.*?)</pre>");
     @Override
