b7dd5487413b5b94bdc92577ca21eca42b3a111f
asters1 <gj744559140@163.com>
2022-11-25 13:10:56 +0000

    js

--- app/src/main/java/com/github/catvod/spider/JS.java
+++ app/src/main/java/com/github/catvod/spider/JS.java
@@ -26,7 +26,7 @@ public class JS extends Spider {
     public String homeContent(boolean filter) {
         //V8 v8 =V8.createV8Runtime();
         //Object result = v8.executeScript("var json = '{\"class\":[{\"type_id\"：\"dianying\",\"type_name\": \"电影\"},{\"type_id\"：\"lianxuju\",\"type_name\": \"连续剧\"}]}'\n;json;");
-        v8.close();
+//        v8.close();
         return "{\"class\":[{\"type_id\"：\"dianying\",\"type_name\": \"电影\"},{\"type_id\"：\"lianxuju\",\"type_name\": \"连续剧\"}]}";
     }
 
