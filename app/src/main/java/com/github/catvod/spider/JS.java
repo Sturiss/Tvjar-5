267c6bbb0c1920a1a287c33b1abe20762ffc825f
asters1 <97214636+asters1@users.noreply.github.com>
2022-11-25 19:44:49 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/catvod/spider/JS.java
@@ -0,0 +1,38 @@
+package com.github.catvod.spider;
+
+import android.content.Context;
+
+import com.github.catvod.crawler.Spider;
+import com.eclipsesource.v8.V8;
+
+import com.github.catvod.crawler.SpiderDebug;
+import com.github.catvod.utils.Misc;
+import com.github.catvod.utils.okhttp.OkHttpUtil;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class JS extends Spider {
+    public static void print(){
+        System.out.println("====================" +
+                           "===     调  试    ===" +
+                           "====================");
+    }
+    public String homeContent(boolean filter) {
+        V8 v8 =V8.createV8Runtime();
+        Object result = v8.executeScript("var json = '{\"class\":[{\"type_id\"：\"dianying\",\"type_name\": \"电影\"},{\"type_id\"：\"lianxuju\",\"type_name\": \"连续剧\"}]}'\n;json;");
+        v8.close();
+        return result.toString();
+    }
+
+
+
+}
+
